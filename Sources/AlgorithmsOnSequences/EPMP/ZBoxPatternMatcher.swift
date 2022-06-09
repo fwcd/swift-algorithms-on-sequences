9a534adf13f64bd368c3d2f25654e43007edbe21
fwcd <fwcdmail@gmail.com>
2022-06-10 00:35:25 +0200

    Stub out Z-algorithm

--- Sources/AlgorithmsOnSequences/EPMP/ZBoxPatternMatcher.swift
+++ Sources/AlgorithmsOnSequences/EPMP/ZBoxPatternMatcher.swift
@@ -1,22 +1,22 @@
-/*
-
 /// A linear time pattern matcher.
 public struct ZBoxPatternMatcher: ExactPatternMatcher {
     public static func findAllOccurrences<Element>(of pattern: [Element], in text: [Element]) -> [Int] where Element: Equatable {
-        let prefixTokens = pattern.map(Token.element) + [.boundary]
-        let tokens = prefixTokens + text.map(Token.element)
+        // Concatenate pattern, boundary and text
+        let tokens = pattern.map(Token.element) + [.boundary] + text.map(Token.element)
+
+        // Compute Z-boxes by finding the longest common prefixes at each index with itself
         let lcps = findLongestCommonPrefixes(in: tokens)
 
-        var occurrences: [Int] = []
-        var i: C.Index = prefixTokens.endIndex
-
-        return occurrences
+        // Use Z-boxes to compute the actual occurrences
+        return (0..<text.count)
+            .filter { lcps[$0 + pattern.count + 1] == pattern.count }
     }
 
-    public static func findLongestCommonPrefixes<C>(in text: C) -> [C.SubSequence]
+    private static func findLongestCommonPrefixes<C>(in text: C) -> [Int]
         where C: Collection,
               C.Element: Equatable {
         // TODO
+        return []
     }
 }
 
@@ -27,5 +27,3 @@ private enum Token<Element> {
 }
 
 extension Token: Equatable where Element: Equatable {}
-
-*/
