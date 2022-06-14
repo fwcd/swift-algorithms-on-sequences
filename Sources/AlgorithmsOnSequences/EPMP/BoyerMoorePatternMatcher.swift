ee9b7cf777d73e722cf4413275d818f80e19c636
fwcd <fwcdmail@gmail.com>
2022-06-14 19:24:24 +0200

    Stub out Boyer-Moore pattern matcher

---
+++ Sources/AlgorithmsOnSequences/EPMP/BoyerMoorePatternMatcher.swift
@@ -0,0 +1,7 @@
+/// An efficient string search algorithm developed by Robert S. Boyer
+/// and J Strother Moore in 1977 that preprocesses the pattern.
+public struct BoyerMoorePatternMatcher: ExactPatternMatcher {
+    public static func findAllOccurrences<Element>(of pattern: [Element], in text: [Element]) -> [Int] where Element: Equatable {
+        fatalError("TODO")
+    }
+}
