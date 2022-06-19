59750a1ef4df1c1fcfa583bd87fd313521e75709
fwcd <fwcdmail@gmail.com>
2022-06-19 03:54:38 +0200

    Add ESMP protocol

---
+++ Sources/AlgorithmsOnSequences/ESMP/ExactSetMatcher.swift
@@ -0,0 +1,19 @@
+/// An algorithm that finds all occurrences of multiple strings
+/// (patterns) in a text. The matcher is initialized with the
+/// set to allow preprocessing.
+public protocol ExactSetMatcher {
+    associatedtype Element: Hashable
+
+    /// Creates a new set matcher, possibly preprocessing
+    /// the given patterns.
+    /// 
+    /// - Parameter patterns: The patterns to search for
+    init(patterns: Set<Element>)
+
+    /// Finds all occurrences of any pattern in the given
+    /// text.
+    /// 
+    /// - Parameter text: The text to search in
+    /// - Returns: The indices at which any pattern from the set occurs
+    func findAllOccurrences(in text: [Element]) -> [Int]
+}
