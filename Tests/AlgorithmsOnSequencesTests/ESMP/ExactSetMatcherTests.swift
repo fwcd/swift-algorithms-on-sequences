6ccfcfcb4eb9276b1d96009a1499f74352e57c8a
fwcd <fwcdmail@gmail.com>
2022-06-21 05:29:05 +0200

    Test ExactSetMatcher

---
+++ Tests/AlgorithmsOnSequencesTests/ESMP/ExactSetMatcherTests.swift
@@ -0,0 +1,34 @@
+import XCTest
+@testable import AlgorithmsOnSequences
+
+final class ExactSetMatcherTests: XCTestCase {
+    func testNaiveSetMatcher() {
+        testExactSetMatcher(NaiveSetMatcher.self)
+    }
+
+    private func testExactSetMatcher<M>(_ type: M.Type)
+        where M: ExactSetMatcher,
+              M.Element == Character {
+        assertThat(type, finds: [], in: "abcabc", at: [])
+
+        assertThat(type, finds: ["abc"], in: "abcabc", at: [0, 3])
+        assertThat(type, finds: ["aa"], in: "aaaaa", at: [0, 1, 2, 3])
+        assertThat(type, finds: ["def"], in: "feddEf", at: [])
+        assertThat(type, finds: [""], in: "test", at: [0, 1, 2, 3])
+        assertThat(type, finds: ["ba"], in: "abba", at: [2])
+        assertThat(type, finds: ["ba"], in: "abbaabba", at: [2, 6])
+        assertThat(type, finds: ["aaa"], in: "abaaa", at: [2])
+
+        assertThat(type, finds: ["ba", "bba"], in: "abbaabba", at: [1, 2, 5, 6])
+        assertThat(type, finds: ["abb", "a", "aab"], in: "abbaabba", at: [0, 3, 4, 7])
+        assertThat(type, finds: ["abb", "aab"], in: "abbaabba", at: [0, 3, 4])
+
+        // TODO: Automatically generate test cases
+    }
+
+    private func assertThat<M>(_ type: M.Type, finds patterns: Set<String>, in text: String, at indices: [Int], line: UInt = #line)
+        where M: ExactSetMatcher,
+              M.Element == Character {
+        XCTAssertEqual(M.init(patterns: Set(patterns.map { Array($0) })).findAllOccurrences(in: Array(text)), indices, line: line)
+    }
+}
